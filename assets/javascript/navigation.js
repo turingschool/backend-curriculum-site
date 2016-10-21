df94379763b7ebe29ac51e7eda2b5628859cdd3e
Andrew Carmer <carmerandrew@gmail.com>
2016-10-21 11:39:20 -0600

    update navigation

---
+++ assets/javascript/navigation.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  toggleMenuItems();
+})
+
+function toggleMenuItems() {
+  $(".click-to-expand").on("click", function(e){
+    e.preventDefault
+    $(this).next().toggle()
+  })
+}
