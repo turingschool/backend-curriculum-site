ab5d57ea39d70f26efaa341813b08d90575570a5
Kat Brandt <96136707+KatBrandt@users.noreply.github.com>
2023-03-28 14:42:18 -0600

    Create expander.js

---
+++ public/js/expander.js
@@ -0,0 +1,18 @@
+$('.answer > h3').each(function(idx, title) {
+  const titleText = $(title).text();
+  $(title).replaceWith(`<h3><img class="expander-arrow" src="/assets/images/arrow.svg" alt="expander arrow" /> ${titleText}</h3>`);
+});
+
+$('.answer > h3').nextAll().hide();
+
+$('.answer > h3').on('click', function() {
+  let arrowEl = $(this).find('img.expander-arrow');
+  
+  if (!arrowEl.hasClass('expanded')) {
+    arrowEl.addClass('expanded');
+    arrowEl.parent('h3').nextAll().show(150);
+  } else {
+    arrowEl.removeClass('expanded');
+    arrowEl.parent('h3').nextAll().hide(150);
+  }
+});
